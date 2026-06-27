429bdae11cdb6395321f0fea0f3d58dbddbdefec
elfathanzioherlambang97 <elfathanzioherlambang@gmail.com>
2026-06-27 22:32:48 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://wflajmjnltdleifvqvtg.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://wflajmjnltdleifvqvtg.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmbGFqbWpubHRkbGVpZnZxdnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDE2NTEsImV4cCI6MjA5NjU3NzY1MX0.sI0Od0mSSqUTvGDKTOTWS5SgkKAgwD1ifrtw5XvsFYE'
 
 const { createClient } = supabase
