a154d7dd10245f5ae76a0ccdbb763de69636df4d
elfathanzioherlambang97 <elfathanzioherlambang@gmail.com>
2026-06-27 22:29:04 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://hahjbyevjggcfbxdsnge.supabase.co/rest/v1/'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhaGpieWV2amdnY2ZieGRzbmdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTQzMjIsImV4cCI6MjA5NzIzMDMyMn0.9xwV0ejQbameioX5BV1Ha9XG8V_k6ulbVSpJTWEBx8Q'
+const SUPABASE_URL = 'https://wflajmjnltdleifvqvtg.supabase.co/rest/v1/'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmbGFqbWpubHRkbGVpZnZxdnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDE2NTEsImV4cCI6MjA5NjU3NzY1MX0.sI0Od0mSSqUTvGDKTOTWS5SgkKAgwD1ifrtw5XvsFYE'
 
 const { createClient } = supabase
 const db = createClient(SUPABASE_URL, SUPABASE_KEY)
